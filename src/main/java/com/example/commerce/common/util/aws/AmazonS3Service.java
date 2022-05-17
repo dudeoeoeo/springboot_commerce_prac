2f8f3170d8efa6275cdf1b72304d8189edef7b7b
dudeoeoeo <dudekekek@naver.com>
2022-05-18 06:11:53 +0900

    s3 추가

---
+++ src/main/java/com/example/commerce/common/util/aws/AmazonS3Service.java
@@ -0,0 +1,65 @@
+package com.example.commerce.common.util.aws;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class AmazonS3Service {
+
+    @Value("${cloud.aws.front.domain}")
+    private String frontUrl;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    private final AmazonS3 amazonS3;
+
+    public List<String> uploadFiles(List<MultipartFile> multipartFiles, String folderName) {
+        List<String> filePaths = new ArrayList<>();
+
+        multipartFiles.forEach(file -> {
+            String fileName = createFileName(folderName, file.getOriginalFilename());
+
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentLength(file.getSize());
+            objectMetadata.setContentType(file.getContentType());
+
+            try (InputStream inputStream = file.getInputStream()) {
+                amazonS3.putObject(new PutObjectRequest(bucketName, fileName, inputStream, objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+            } catch (IOException e) {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다. " + file.getOriginalFilename());
+            }
+
+            filePaths.add(frontUrl + fileName);
+        });
+
+        return filePaths;
+    }
+
+    public String createFileName(String folderName, String originFileName) {
+        return folderName + "/" + UUID.randomUUID().toString().contains(getFileExtension(originFileName));
+    }
+
+    public String getFileExtension(String originFileName) {
+        try {
+            return originFileName.substring(originFileName.lastIndexOf("."));
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 파일 형식 '" + originFileName + "' 입니다.");
+        }
+    }
+}
