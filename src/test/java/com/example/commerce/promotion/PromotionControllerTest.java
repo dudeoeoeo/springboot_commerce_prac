b28f5522d88b91e9f2af6596e15699bf0359fc0c
dudeoeoeo <dudekekek@naver.com>
2022-07-30 17:46:15 +0900

    promotion add test

---
+++ src/test/java/com/example/commerce/promotion/PromotionControllerTest.java
@@ -0,0 +1,48 @@
+package com.example.commerce.promotion;
+
+import com.example.commerce.business.auth.util.JwtProperties;
+import com.example.commerce.business.item.domain.Item;
+import com.example.commerce.business.user.domain.User;
+import com.example.commerce.config.RestDocsTestSupport;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+
+public class PromotionControllerTest extends RestDocsTestSupport {
+
+    private final String PREFIX = "/api/v1/promotion";
+
+    @Test
+    @Transactional
+    void addPromotion() throws Exception {
+        final User user = userSave();
+        final Item item = addItem();
+        final String token = getTokenByUser(user);
+
+        Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("itemOptionId", item.getOptions().get(0).getId());
+        requestMap.put("discountPercent", 5.0);
+        requestMap.put("stock", 20);
+        requestMap.put("useCoupon", true);
+        requestMap.put("usePoint", false);
+        requestMap.put("startDate", LocalDate.now());
+        requestMap.put("endDate", LocalDate.now().plusWeeks(3));
+
+        mockMvc.perform(
+                post(PREFIX + "/add")
+                .header(JwtProperties.HEADER_STRING, token)
+                .content(createJson(requestMap))
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
